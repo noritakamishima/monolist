a49a09268e136a0b3bc316ab7d906f0ee2685271
noritakamishima <noritaka.mishima@gmail.com>
2015-10-08 03:28:57 +0000

    2nd kadai

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ class Item < ActiveRecord::Base
 
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+# ここから追記（6.7の4項目関連）
+  has_many :wants, class_name: "Want", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users, through: :wants, source: :user
+  
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users, through: :haves, source: :user
+# ここまで追記（6.7の4項目関連）
+
+
 end
